4ce9223b9e3be12e97fa07056cba61955be752fc
Bhavana266 <bhavana26602@gmail.com>
2020-12-15 13:29:56 +0530

    Add files via upload

---
+++ BBMSDriver.java
@@ -0,0 +1,7 @@
+public class BBMSDriver {
+
+	public static void main(String[] args) throws Exception {
+		new InterfaceDesign();
+	}
+
+}
---
+++ User.java
@@ -0,0 +1,7 @@
+
+public interface User {
+	public void add() throws Exception;
+	public void delete(String n) throws Exception;
+	public void edit(String n) throws Exception;
+	public void search(String n) throws Exception;
+}
